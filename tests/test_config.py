7be1a5122128cf89798fb139999ec85bc13a8187
mukelwe-m <mukelwemdluli@gmail.com>
2026-03-17 13:57:04 +0200

    Made a small change to trigger workflow

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,7 @@
 # Just check if the student has loaded the environment variables correctly
 from helpers.config import Config
 
-
+# Making a simple test to check if the environment variables are loaded correctly
 def test_currency_symbol():
     assert Config.get_currency_symbol() == "R"
 
